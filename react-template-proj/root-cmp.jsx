f0d529fec618bde7d876ced53fbf0973032cecdb
Your Name <dan.weibren@gmail.com>
2025-06-12 08:07:07 +0300

    feat(ui): add RootCmp component with integrated feature modules
    introduced RootCmp component as main root layout integrating the following subcomponents: AnimalList (predefined animal list), SeasonalClock (dynamic clock), CountDown (20-second timer), WatcherAppIndex (watcher app UI), MouseMonitorIndex (live mouse monitor display)

--- react-template-proj/root-cmp.jsx
+++ react-template-proj/root-cmp.jsx
@@ -1,12 +1,25 @@
-export function App() {
+import { AnimalList } from './cmps/animalAppCmps/AnimalList.jsx'
+import { SeasonalClock } from './cmps/SeasonalClock.jsx'
+import { CountDown } from './cmps/CountDown.jsx'
+import { WatcherAppIndex } from './cmps/watcherAppCmps/WatcherAppIndex.jsx'
+import { MouseMonitorIndex } from './cmps/mouseMonitorApp/MouseMonitorIndex.jsx'
+
+export function RootCmp() {
+    const animalInfos = [
+        { type: 'Malayan Tiger', count: 787 },
+        { type: 'Mountain Gorilla', count: 212 },
+        { type: 'Fin Whale', count: 28 },
+    ]
+
     return (
-        <section className="app">
-            <header className="app-header">
-                <h1>My App</h1>
-            </header>
-            <main className="container">
-                Hello React App
-            </main>
-        </section>
+        <div className='main-root-layout'>
+            <AnimalList animalInfos={animalInfos} />
+            <SeasonalClock />
+            <CountDown toTime={Date.now() + 1000 * 20} startFrom={10} onDone={() => {
+                console.log('Done!')
+            }} />
+            <WatcherAppIndex />
+            <MouseMonitorIndex />
+        </div>
     )
 }
