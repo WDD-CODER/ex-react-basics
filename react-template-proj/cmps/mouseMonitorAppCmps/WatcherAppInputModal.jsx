79a09222c4607440adc11d457aa095d060bda4dc
Your Name <dan.weibren@gmail.com>
2025-06-12 08:23:07 +0300

    feat(watcherApp): add WatcherAppInputModal component for adding new watcher with full name and movies
    added WatcherAppInputModal component to capture new watcher details; supports entering full name and multiple movies with onBlur event; triggers onAddUser callback with constructed user object on save

---
+++ react-template-proj/cmps/mouseMonitorAppCmps/WatcherAppInputModal.jsx
@@ -0,0 +1,41 @@
+import { appService } from '../../services/watcherApp.service.js'
+const { useState, useEffect, } = React;
+
+export function WatcherAppInputModal({ onAddUser }) {
+
+    const [user, setUser] = useState({ fullName: '', movies: [] })
+
+
+    function setFullName(value) {
+        setUser(user => ({ ...user, fullName: value }))
+    }
+
+    function setUserMovie(value) {
+        setUser(user => ({ ...user, movies: [...user.movies, value] }))
+    }
+
+    return (
+        <React.Fragment>
+            <ul>
+                <label className="fullName input">What's the user's full name? </label>
+                <br />
+                <li>
+                    <input id="fullName" type='text' onBlur={ev => setFullName(ev.target.value)} />
+                </li>
+                <br />
+                <label className="movie-1 input">What movie did the user watch?</label>
+                <br />
+                <li>
+                    <input type='text' onBlur={ev => setUserMovie(ev.target.value)} />
+                </li>
+                <br />
+                <label className="movie-1 input">What movie Did the user watch?</label>
+                <br />
+                <li>
+                    <input type='text' onBlur={ev => setUserMovie(ev.target.value)} />
+                </li>
+            </ul>
+            <button onClick={() => onAddUser(user)}>Save User</button>
+        </React.Fragment>
+    )
+}
